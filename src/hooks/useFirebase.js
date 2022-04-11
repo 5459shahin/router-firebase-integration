a6773b6273eab3c38e3efd7c7a9718820864de2f
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-04-11 23:17:44 +0800

    first commit

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { useEffect, useState } from "react";
+import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from 'firebase/auth';
 import app from '../firebase.init';
 
 const auth = getAuth(app);
@@ -16,11 +16,23 @@ const useFirebase = () => {
            setUser(user);
            console.log(user);
        })
-
    }
-//    return [user, setUser]
+
+   const handleSignOut = () =>{
+        signOut(auth)
+        .then(() =>{})
+   }
+
+   useEffect( () => {
+       onAuthStateChanged(auth, user =>{
+           setUser(user);
+       })
+   }, [])
+
+ //    return [user, setUser]
    return {
        user,
+       handleSignOut,
         singInWithGoogle
     }
 };
